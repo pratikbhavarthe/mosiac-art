6f856d09a60ae62a8006ea0bfdda500619d70adf
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2025-02-20 15:58:59 +0530

    Add generateMosaic function to create mosaic grid with emoji mapping

--- lib/mosiacGeneration.ts
+++ lib/mosiacGeneration.ts
@@ -1,2 +1,10 @@
 import { processImage } from "./imageProcessing";
 import { findClosestEmoji } from "./emojiMapping";
+
+export async function generateMosaic(imagePath: string, gridSize = 128) {
+    const grid = await processImage(imagePath, gridSize);
+    
+    return grid.map(cell => ({
+        ...cell,  emoji: findClosestEmoji({ r: cell.r, g: cell.g, b: cell.b })?.char
+    }));
+}
